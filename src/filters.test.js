40ac1ce0227ef2bca7b0ec9f45a5fc58671c61a4
Zsolt Dollenstein <zsol.zsol@gmail.com>
2017-02-26 14:15:20 +0000

    add more tests

--- src/filters.test.js
+++ src/filters.test.js
@@ -9,3 +9,19 @@ test('empty config strips dollar prefix and newline postfix', () => {
 test('dollar prefix strip leaves variables in place', () => {
   expect(filterPasteData({keepDollarPrefixes: false}, '$PATH')).toBe('$PATH');
 });
+
+test('allows keeping dollar prefixes', () => {
+  expect(filterPasteData({keepDollarPrefixes: true}, ' $ yes')).toBe(' $ yes');
+});
+
+test('allows keeping last newline', () => {
+  expect(filterPasteData({keepNewlinePostfix: true}, 'data\n')).toBe('data\n');
+});
+
+test('strips all newlines from end', () => {
+  expect(filterPasteData({keepNewlinePostfix: false}, 'data\n\n')).toBe('data');
+});
+
+test('allows passing in custom filter function', () => {
+  expect(filterPasteData({overrideFilter: () => 'yolo'}, 'meh')).toBe('yolo');
+});
