63eccfd05a41db2b11d529b2e8f55d8f3662d9ae
Thiago Mendonca <trystan2k@gmail.com>
2021-06-22 07:17:21 +0200

    docs: update documentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true,
-        "jest": true
-    },
-    "extends": ["eslint:recommended", "plugin:react/recommended"],
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "sourceType": "module",
-        "ecmaFeatures": {
-          "jsx": true
-        }
-    },
-    "plugins": [
-      "react",
-      "flowtype"
-    ],
-    "rules": {
-      "indent": [
-          "error",
-          2
-      ],
-      "linebreak-style": [
-          "error",
-          "unix"
-      ],
-      "quotes": [
-          "error",
-          "single"
-      ],
-      "semi": [
-          "error",
-          "always"
-      ],
-      "no-unused-vars": [
-        "error",
-        {
-          "argsIgnorePattern": "^_"
-        }
-      ]
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:json/recommended"],
+  parser: "babel-eslint",
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
     }
+  },
+  plugins: ["react", "flowtype", "json"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_"
+      }
+    ]
+  }
 };
