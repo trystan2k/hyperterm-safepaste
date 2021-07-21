663c3217a7d2d4d43c42d0fc46b1e06d1e39a480
Thiago Mendonca <trystan2k@gmail.com>
2021-07-21 18:49:33 +0200

    feat: refactor to avoid modify paste event directly

--- src/PasteDialog.react.js
+++ src/PasteDialog.react.js
@@ -1,19 +1,20 @@
 // @flow
 
-import React from 'react';
+import type {Node} from 'react';
+import React, { Component } from 'react';
 import invariant from 'invariant';
 
 type Props = {
   continuePasting?: (string) => void,
-  onExit?: () => void,
-  pasteData?: string,
+  onExit?: (Event) => void,
+  pasteData: string,
 }
 
 type State = {
   editedPaste: string,
 }
 
-export default class PasteDialog extends React.Component {
+export default class PasteDialog extends Component<Props, State> {
   state: State;
   buttonFocused: ?bool;
 
@@ -26,15 +27,16 @@ export default class PasteDialog extends React.Component {
     this.buttonFocused = false;
   }
 
-  componentWillReceiveProps(props: Props) {
-    this.setState({
-      editedPaste: props.pasteData || '',
-    });
-  }
-
-  escFunction = (e: KeyboardEvent) => {
+  escFunction: KeyboardEvent => void = (e) => {
     if ((e.key === 'Escape' || e.key === 'Esc') && this.props.onExit) {
       this.props.onExit(e);
+    }
+  }
+  componentDidUpdate({pasteData}: Props){
+    if (this.props.pasteData !== pasteData) {
+      this.setState({
+        editedPaste: this.props.pasteData || '',
+      });    
     }
   }
   componentDidMount(){
@@ -44,7 +46,7 @@ export default class PasteDialog extends React.Component {
     window.removeEventListener('keydown', this.escFunction, {capture: false});
   }  
 
-  doPaste = () => {
+  doPaste: void => void = () => {
     const data = this.state.editedPaste;
     const paste = this.props.continuePasting;
     invariant(data, 'paste data must be set');
@@ -52,14 +54,14 @@ export default class PasteDialog extends React.Component {
     paste(data);
   }
 
-  focus = (elem: HTMLElement) => {
+  focus: ?HTMLElement => void = (elem) => {
     if (this.buttonFocused === true) {
-      elem.focus();
+      elem?.focus();
       this.buttonFocused = false;
     }
   }
 
-  render() {
+  render(): Node {
     if (this.props.pasteData == null || this.props.continuePasting == null) {
       this.buttonFocused = null;
       return <div />;
@@ -80,48 +82,48 @@ export default class PasteDialog extends React.Component {
       alignItems: 'center',
       justifyContent: 'center'
     }}>
-        <div style={{
-          padding: '8px 14px 9px',
-          transition: '150ms opacity ease',
-          fontSize: config.fontSize + 'px',
-          background: 'rgba(128, 128, 128, 0.8)',
-          display: 'flex',
-          flexDirection: 'column',
-          boxShadow: '3px 13px 73px 14px rgba(0,0,0,0.65)',
-          borderRadius: '10px'
+      <div style={{
+        padding: '8px 14px 9px',
+        transition: '150ms opacity ease',
+        fontSize: config.fontSize + 'px',
+        background: 'rgba(128, 128, 128, 0.8)',
+        display: 'flex',
+        flexDirection: 'column',
+        boxShadow: '3px 13px 73px 14px rgba(0,0,0,0.65)',
+        borderRadius: '10px'
+      }}>
+        <span style={{
+          fontSize: config.fontSize * 1.2 + 'px',
+          fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+          color: config.foregroundColor,
         }}>
-          <span style={{
-            fontSize: config.fontSize * 1.2 + 'px',
-            fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+          You have a chance to edit your multiline paste
+          before it reaches the terminal. Esc cancels.
+        </span>
+        <textarea id='paste-data'
+          rows={this.state.editedPaste.split('\n').length}
+          style={{
+            margin: '1em 0',
+            background: config.backgroundColor,
             color: config.foregroundColor,
+            fontSize: config.fontSize,
+            borderColor: config.borderColor,
+            fontFamily: config.fontFamily,
+          }}
+          onChange={e => this.setState({editedPaste: e.target.value})}
+          value={this.state.editedPaste}
+        />
+        <div style={{textAlign: 'center'}} >
+          <button id='paste' onClick={this.doPaste} ref={elem => this.focus(elem)} style={{
+            background: 'transparent',
+            border: 'none',
+            width: '20px',
+            height: '20px'
           }}>
-            You have a chance to edit your multiline paste
-            before it reaches the terminal. Esc cancels.
-          </span>
-          <textarea id='paste-data'
-            rows={this.state.editedPaste.split('\n').length}
-            style={{
-              margin: '1em 0',
-              background: config.backgroundColor,
-              color: config.foregroundColor,
-              fontSize: config.fontSize,
-              borderColor: config.borderColor,
-              fontFamily: config.fontFamily,
-            }}
-            onChange={e => this.setState({editedPaste: e.target.value})}
-            value={this.state.editedPaste}
-          />
-          <div style={{textAlign: 'center'}} >
-            <button id='paste' onClick={this.doPaste} ref={elem => this.focus(elem)} style={{
-              background: 'transparent',
-              border: 'none',
-              width: '20px',
-              height: '20px'
-            }}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" fill={config.foregroundColor}><g><path d="M720.9,396.3c-13.3-8.4-30.9-4.5-39.3,8.8L477.2,728.2L343.8,620.8c-12.2-9.9-30.2-7.9-40,4.5c-9.8,12.4-7.8,30.4,4.4,40.3l158.1,127.2c5.1,4.1,11.3,6.3,17.8,6.3c1.6,0,3.1-0.1,4.7-0.4c8-1.3,15-6,19.3-12.9l221.4-350C738,422.5,734.1,404.8,720.9,396.3z"/><path d="M768.8,141.4h-84.3v-26h-78.2C593.4,60.7,547,10,488.8,10c-58.2,0-108,50.7-120.8,105.4h-78.2v26h-58.5c-85.4,0-155,65.8-155,151.8v540.9c0,85.9,69.5,155.9,155,155.9h537.6c85.4,0,155-70,155-155.9V293.2C923.8,207.2,854.3,141.4,768.8,141.4z M419.9,166.9v-32.8c0-36.8,32.3-71.7,68.9-71.7c36.6,0,63.9,34.9,63.9,71.7v32.8h79.1v133.6H340V166.9H419.9z M869.4,834.1c0,54.4-46.5,102.8-100.5,102.8H231.2c-54.1,0-104.7-48.4-104.7-102.8V293.2c0-54.4,50.6-98.6,104.7-98.6h58.5v159.1h394.9V194.6h84.3c54.1,0,100.5,44.3,100.5,98.6L869.4,834.1L869.4,834.1z"/></g></svg>
-            </button>
-          </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" fill={config.foregroundColor}><g><path d="M720.9,396.3c-13.3-8.4-30.9-4.5-39.3,8.8L477.2,728.2L343.8,620.8c-12.2-9.9-30.2-7.9-40,4.5c-9.8,12.4-7.8,30.4,4.4,40.3l158.1,127.2c5.1,4.1,11.3,6.3,17.8,6.3c1.6,0,3.1-0.1,4.7-0.4c8-1.3,15-6,19.3-12.9l221.4-350C738,422.5,734.1,404.8,720.9,396.3z"/><path d="M768.8,141.4h-84.3v-26h-78.2C593.4,60.7,547,10,488.8,10c-58.2,0-108,50.7-120.8,105.4h-78.2v26h-58.5c-85.4,0-155,65.8-155,151.8v540.9c0,85.9,69.5,155.9,155,155.9h537.6c85.4,0,155-70,155-155.9V293.2C923.8,207.2,854.3,141.4,768.8,141.4z M419.9,166.9v-32.8c0-36.8,32.3-71.7,68.9-71.7c36.6,0,63.9,34.9,63.9,71.7v32.8h79.1v133.6H340V166.9H419.9z M869.4,834.1c0,54.4-46.5,102.8-100.5,102.8H231.2c-54.1,0-104.7-48.4-104.7-102.8V293.2c0-54.4,50.6-98.6,104.7-98.6h58.5v159.1h394.9V194.6h84.3c54.1,0,100.5,44.3,100.5,98.6L869.4,834.1L869.4,834.1z"/></g></svg>
+          </button>
         </div>
-      </div>;
+      </div>
+    </div>;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 import filterPasteData from './filters';
 import {fromObject as configFromObject} from './config';
 import PasteDialog from './PasteDialog.react';
@@ -10,11 +12,19 @@ type State = {
 type Props = {
   onActive: ?Function,
   onDecorated: ?Function,
-  customChildren: ?Array,
+  customChildren: ?Array<*>,
+  activeSession: ?string
 }
 
-export function decorateTerms(Terms, { React }) {
-  return class extends React.Component {
+export function onWindow(win: any) {
+  win.rpc.on('pasted-data', ({data, sessionId}) => {
+    const session = win.sessions.get(sessionId);
+    session.write(data);
+  });
+}
+
+export function decorateTerms(Terms: any , { React }: any): any {
+  return class SafePasteComponent extends React.Component<Props, State> {
     state: State;
     constructor(props: Props, context) {
       super(props, context);
@@ -23,9 +33,12 @@ export function decorateTerms(Terms, { React }) {
       this._element = null;
       this.state = {};
     }
+  
 
+    _onDecorated: Function;
     _onDecorated(terms) {
       if (this.props.onDecorated) this.props.onDecorated(terms);
+      this._terms = terms;
       
       window.addEventListener('paste', this._onSafePaste, {capture: true});
     }
@@ -36,35 +49,30 @@ export function decorateTerms(Terms, { React }) {
       this._element = document.activeElement;
     }
 
+    _onSafePaste: Function;
     _onSafePaste(e) {
 
-      if (e.hypertermSafePasteMockPasteEvent) {
-        return;
-      }
       const pastedData = e.clipboardData.getData('text');
-      e.stopPropagation();
-      e.preventDefault();
-      const pasteCallback = (saferData, noFocus) => {
-        const modifiedPasteEvent = Object.assign(new Event('paste'), {
-          clipboardData: {
-            getData: _ => saferData,
-          },
-          hypertermSafePasteMockPasteEvent: true,
-        });
-        this.resetState();
-        e.target.dispatchEvent(modifiedPasteEvent);
-
-        if (!noFocus && this._element) {
-          this._element.focus();
-        }
-      };
       const filtered = filterPasteData(
         configFromObject(window.config.getConfig()),
         pastedData,
       );
       if (!filtered.includes('\n')) {
-        return pasteCallback(filtered, true);
-      }
+        return;
+      }   
+
+      e.stopPropagation();
+      e.preventDefault();
+
+      const pasteCallback = (saferData, noFocus) => {
+        this.resetState();
+        window.rpc.emit('pasted-data', {data: saferData, sessionId: this.props.activeSession});
+
+        if (!noFocus && this._element) {
+          this._element.focus();
+        }        
+      };
+      
       this.setState({
         pastedData,
         pasteCallback,
@@ -86,13 +94,11 @@ export function decorateTerms(Terms, { React }) {
     }
 
     render() {
-
       const customChildren = Array.from(this.props.customChildren || [])
-      .concat(React.createElement(PasteDialog, Object.assign({}, {pasteData: this.state.pastedData, continuePasting: this.state.pasteCallback,
-        onExit: this._onExit})));
+        .concat(React.createElement(PasteDialog, Object.assign({}, {pasteData: this.state.pastedData, continuePasting: this.state.pasteCallback,
+          onExit: this._onExit})));
 
       return React.createElement(Terms, Object.assign({}, this.props, {onActive: this._onActive, onDecorated: this._onDecorated, customChildren: customChildren}));
     }
-
   };
 }
